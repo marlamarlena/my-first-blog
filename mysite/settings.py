cb64b4ec9b7bb8d4084a0ccf65cf8c680ce3615d
Marlena <marlena.hlebowicz@wp.pl>
2016-05-21 14:09:41 +0200

    fix typo in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,4 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATISTIC_ROOT=os.path.join(BASE_DIR,'public','statistic')
+STATIC_ROOT=os.path.join(BASE_DIR,'public','statistic')
